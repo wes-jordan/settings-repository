386c6e8aac545fae3ca64c4fd79dd7cee63952c5
Wes Jordan <wesley@wealthbridgemarketing.com>
2016-08-24 06:35:37 -0600

    removing files no longer needed

--- Documents/MySQL/DB Cleanse.sql
+++
@@ -1,5 +0,0 @@
-UPDATE Younique.emails SET email='wesley@wealthbridgemarketing.com';
-UPDATE Younique.presenters SET government_id='111-11-1111';
-UPDATE Younique.order_shipments SET email_address='wesley@wealthbridgemarketing.com';
-DROP INDEX `email_UNIQUE` ON Younique.users;
-UPDATE Younique.users SET email='wesley@wealthbridgemarketing.com';
--- Documents/MySQL/Replacement Orders.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM Younique.orders 
-WHERE order_market_id=1
-AND order_status_id IN (3,5,8)
-HAVING order_type_id=7
-ORDER BY id DESC;
